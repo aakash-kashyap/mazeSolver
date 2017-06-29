6f1f9c9c4bf395bf5faf3cf4dae9fe8678c408d1
aakash-kashyap <aakashkashyap1996@gmail.com>
2017-06-29 19:40:15 +0530

    changes some comments

--- script.py
+++ script.py
@@ -1,23 +1,21 @@
+## Importing necessary packages.
 import numpy as np
 import cv2
 
 ## The readImage function takes a file path as argument and returns image in binary form.
 def readImage(filePath):
-    #############  Add your Code here   ###############
+    
     img = cv2.imread(filePath)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     ret,binaryImage = cv2.threshold(gray,200,255,0)
 ##    cv2.imshow('Binary Image',binaryImage)
-    ###################################################
     return binaryImage
 
 ## The findNeighbours function takes a maze image and row and column coordinates of a cell as input arguments
 ## and returns a stack consisting of all the neighbours of the cell as output.
-## Note :- Neighbour refers to all the adjacent cells one can traverse to from that cell provided only horizontal
-## and vertical traversal is allowed.
+
 def findNeighbours(img,row,column):
     neighbours = []
-    #############  Add your Code here   ###############
     h,w = img.shape
     i=(len(img)/20)    
     sImg = img[row*(h/i) : (row*(h/i))+(h/i) , column*(w/i) :(column*(w/i))+(w/i)]
@@ -50,7 +48,6 @@ def findNeighbours(img,row,column):
         ##print "west"
         temp=(row,column-1)
         neighbours.append(temp)
-    ###################################################
     return neighbours
 
 ##  colourCell function takes 4 arguments:-
@@ -58,21 +55,16 @@ def findNeighbours(img,row,column):
 ##            row - row coordinates of cell to be coloured
 ##            column - column coordinates of cell to be coloured
 ##            colourVal - the intensity of the colour.
-##  colourCell basically highlights the given cell by painting it with the given colourVal. Care should be taken that
-##  the function doesn't paint over the black walls and only paints the empty spaces. This function returns the image
+##  colourCell basically highlights the given cell by painting it with the given colourVal.This function returns the image
 ##  with the painted cell.
 def colourCell(img,row,column,colourVal):
-    #############  Add your Code here   ###############
     h,w = img.shape
     i=(len(img)/20)        
     ret,thresh1 = cv2.threshold(img[row*(h/i):(row*(h/i))+h/i,column*(w/i) :(column*(w/i))+w/i ],0,colourVal,cv2.THRESH_BINARY)
     img[row*(h/i):(row*(h/i))+h/i,column*(w/i) :(column*(w/i))+w/i ]=thresh1 
-
-    ###################################################
     return img
 
 ##  Main function takes the filepath of image as input.
-##  You are not allowed to change any code in this function.
 def main(filePath):
     img = readImage(filePath)
     coords = [(0,0),(9,9),(3,2),(4,7),(8,6)]
@@ -88,6 +80,7 @@ def main(filePath):
         return img
     else:
         return string + "\t"
+    
 ## Specify filepath of image here. The main function is called in this section.
 if __name__ == '__main__':
     filePath = 'testMazes/maze06.jpg'
